be84f049b7816e5cf3e0f300b9a7a8fc2aeb36be
Juan Montealegre <98852422+montejs3@users.noreply.github.com>
2025-04-30 20:37:31 -0500

    Update vercel.json (#5)
    * Update vercel.json
    
    * scaffolding
    
    * pr comments

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,8 @@
 import './App.css'
-import NavBar from './navBar'
+import NavBar from './components/navBar'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import About from './about'
+import About from './pages/about'
+import Home from './pages/home';
 
 function App() {
 
@@ -11,7 +12,7 @@ function App() {
       <BrowserRouter>
         <NavBar></NavBar>
         <Routes>
-          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
         </Routes>
       </BrowserRouter>
---
+++ client/src/pages/about.jsx
@@ -0,0 +1,13 @@
+import logoImg from '../assets/logo.png';
+function About() {
+    return (
+        <div>
+            <h1>Welconme to the best data page</h1>
+            <img src={logoImg} alt="Application Logo" />
+        </div>
+
+
+    )
+}
+
+export default About
